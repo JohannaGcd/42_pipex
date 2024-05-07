111857426009161c6a60f3196c3a2a0bd916fdf0
jguacide <guacidejohanna@gmail.com>
2024-05-07 12:16:57 +0200

    progress with parsing + progress with redirecting from/to infile/outfile.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jguacide <jguacide@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 10:55:50 by jguacide          #+#    #+#             */
-/*   Updated: 2024/05/06 16:33:34 by jguacide         ###   ########.fr       */
+/*   Updated: 2024/05/07 12:15:25 by jguacide         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,21 @@
 //
 // Parse the env array to find the PATH substring
 // parse the whole command inbeween quotes, it's not "ls -l" but "ls" "-l"
+// TODO: separate this function so that one returns new_cmd and the other the full path (that one is done already)
 char *get_cmd_path(char *env[], char *cmd)
 {
 	int i;
-
+	char **new_cmd;
+	
 	i = 0;
+	// get single command from argv ("ls -la" become double array with "ls" and "-la")
+	new_cmd = ft_split(cmd, ' ');
+	if (!new_cmd)
+	{
+		ft_free(new_cmd); // check this one, doesn't seem to free all date inside but just to free the double pointer
+	}
+	//TODO: protect split -> create function to free double 
+	
 	while (env[i] != NULL)
 	{
 		// loop through env[][], find PATH substring
@@ -36,19 +46,24 @@ char *get_cmd_path(char *env[], char *cmd)
 	// Skip PATH= and split the string 
 	char *path = env[i] + 5;
 	char **path_dir = ft_split(path, ':');
-	// Test the command
+	//TODO: protect split
+	
+	//Testing the command to find the right path
 	i = 0;
 	while (path_dir[i] != NULL)
 	{
 		char *dir = path_dir[i];
+		// adding "/" after the potential path
 		char *step1_cmd = ft_strjoin(dir, "/");
+		// protecting strjoin in case of failure
 		if (!step1_cmd)
 		{
 			printf("error with first strjoin.");
-			ft_free(path_dir);
+			ft_free(path_dir);// see above, check ft_Free -> not sure it frees all the date inside
 			return (NULL);
 		}
-		char *full_cmd = ft_strjoin(step1_cmd, cmd);
+		// adding the command (index 0) at te end of the path
+		char *full_cmd = ft_strjoin(step1_cmd, new_cmd[0]);
 		if (access(full_cmd, X_OK) == 0)
 		{
 			ft_free(path_dir);
@@ -69,14 +84,14 @@ int main(int argc, char *argv[], char *env[])
 	char *first_cmd;
 	char *second_cmd;
 	// With first command
-	first_cmd = get_cmd_path(env, argv[2]);
+	first_cmd = get_cmd_path(env, argv[3]);
 	if (first_cmd == NULL)
 	{
 		ft_printf("Not valid command");
 		return (1);
 	}
 	// And second one
-	second_cmd = get_cmd_path(env, argv[3]);
+	second_cmd = get_cmd_path(env, argv[4]);
 	if (second_cmd == NULL)
 	{
 		ft_printf("Not a valid command");
@@ -100,16 +115,21 @@ int main(int argc, char *argv[], char *env[])
 	if (id1 == 0)
 	{
 		// In CHILD 
+		int fd_file = open(argv[1], O_WRONLY | O_CREAT, 0777);
+		if (fd_file == -1)
+			return (perror("Error opening file"), 1);
+		if (dup2(STDIN_FILENO, fd_file) == -1)
+			return (perror("Error redirecting STDIN to infile"), 1);
+		if (dup2(fd[1], STDOUT_FILENO) == -1)
+			return (perror("Error redirecting pipe to STDOUT"), 1);
 		// close unused fd: read end
-		// TODO:open file, use fd and redirect it 
-		dup2(fd[1], STDOUT_FILENO);
-		dup2(infile, STDIN_FILENO);
+		close(fd_file); // necessary to close or not?
 		close(fd[0]);
 		close(fd[1]);
-		char *args[] = {first_cmd, NULL};
-		execve(first_cmd, args, NULL);// 3rd argument is the environment list env
-		// TODO: take care of case if exec fails (other code will keep executing)
-		// TODO: I need to cater to the only two commands listed in the subject?
+		char *args[] = {new_cmd[0], new_cmd[1], NULL}; // what if there is no new_cmd 1 ? TODO: populate args with a while loop?
+		if (execve(first_cmd, args, env) == -1)
+			return (perror("Error with execve"), 1);
+		return (0);
 	}
 
 	// Fork second child
@@ -122,15 +142,14 @@ int main(int argc, char *argv[], char *env[])
 	if (id2 == 0)
 	{
 		// in CHILD
-		// close unused fd: write end
-		// dup read end to STDIN 
+		int outfile = open(argv[4], O_RDONLY);
 		dup2(fd[0], STDIN_FILENO);
+		dup2(STDOUT_FILENO, outfile);
 		close(fd[1]);
 		close(fd[0]);
-		// read from STDIN 
-		read(STDIN_FILENO, &, )
-		// TODO: dup to whatever argv[4] was specified as 
-		
+		// TODO: how to knpw what amount to read? use get_next_line?
+		read(STDIN_FILENO, &, sizeof(?));
+		write(outfile, &OUTPUTDEMACMD)
 	}
 
 	// in parent wait for both children to execute and grab error code with waitpid
